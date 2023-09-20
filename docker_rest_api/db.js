2ad7daee8aed168036966216fa6732a0c07a4e27
SammyToro <samolonzy@rocketmail.com>
2023-09-20 10:13:34 +0000

    Added docker rest api folder

---
+++ docker_rest_api/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(
+            "mongodb://root@example@db:27017",
+            {useNewUrlParser: true},
+            {useUnifiedTopology: true}
+        );
+        console.log("MongoDB COnnected")
+    }catch(error){
+        console.log(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
